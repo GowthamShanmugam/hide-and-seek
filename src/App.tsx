72b6d93f6eff19e55450cdebfbd3c664ec71afed
Gowtham Shanmugasundaram <gshanmug@redhat.com>
2022-09-23 16:11:45 +0530

    Basic typescript project
    Signed-off-by: Gowtham Shanmugasundaram <gshanmug@redhat.com>

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { Switch, Route } from "react-router-dom";
+import SessionManager from "./sessionManager";
+import Login from "./pages/login";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/" exact component={SessionManager} />
+        <Route path="/login" exact component={Login} />
+      </Switch>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/login.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Login: React.FC = () => {
+  return <>Hi How are you?</>;
+};
+
+export default Login;
