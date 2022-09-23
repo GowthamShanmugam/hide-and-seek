79ee45128fe87fe3efb1cf5f57da6f184bb24b39
Gowtham Shanmugasundaram <gshanmug@redhat.com>
2022-09-23 16:11:45 +0530

    Basic typescript project
    Signed-off-by: Gowtham Shanmugasundaram <gshanmug@redhat.com>

---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const Login: React.FC = () => {
+    return(
+        <>
+          Hi How are you?
+        </>
+    )
+};
+
+export default Login;
---
+++ src/sessionManager.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { useHistory } from "react-router-dom";
+
+const SessionManager: React.FC =  () => {
+    const history = useHistory();
+    const getSession = () => {
+      const session = localStorage.getItem('nethunt');
+      if(!session) {
+        history.push("/login");
+      } else {
+        history.push("/events");
+      }
+    }
+    
+    return(
+        <> {getSession()}</>
+    );
+}
+
+export default SessionManager;
